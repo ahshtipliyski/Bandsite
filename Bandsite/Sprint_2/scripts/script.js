965ec18a076170e68f2695ac3127460e5c386939
Anton Shtipliyski <antonshtipliyski@yahoo.com>
2020-06-06 23:10:15 -0300

    Mobile completed with style and JS. Tablet and Desktop to be adjusted...

--- Bandsite/Sprint_2/scripts/script.js
+++ Bandsite/Sprint_2/scripts/script.js
@@ -1,197 +1,119 @@
-
-
-const commentsArray = [
+let commentsArray = [
   {
-    imgUrl: "/assets/images/Mohan-muruge.jpg",
-    name: "Michael Lyons",
-    date: "12/18/2018",
-    comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
+  name: "Micheal Lyons",
+  date: "12/18/2018",
+  comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
   },
   {
-    imgUrl: "/assets/images/Mohan-muruge.jpg",
-    name: "Not Michael Lyons",
-    date: "12/18/2018",
-    comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
+  name: "Gary Wong",
+  date: "12/12/2018",
+  comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
   },
   {
-    imgUrl: "/assets/images/Mohan-muruge.jpg",
-    name: "Michael Lyons",
-    date: "12/18/2018",
-    comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
-  }
-];
+  name: "Theodore Duncan",
+  date: "11/15/2018",
+  comment: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"
+}];
 
-let commentsSection = document.querySelector(".comments");
-let parentForm = document.getElementById("comments__form");
-let biggerContainer = document.createElement('div');
-biggerContainer.classList.add("comments__bigger-container");
-commentsSection.appendChild(biggerContainer);
+//creating default comments and pushing information from the array.
+function displayComments(array) {
+let biggerContainer = document.querySelector(".comment__comments-array");
 
-function loopingFunction(commentsArray) {
-for(let i = 0; i < commentsArray.length; i++) {  
-
-  
-  let commentContainer = document.createElement('div');
-  commentContainer.classList.add("comments__container");
+for (let i = 0; i < array.length; i++) {
+  let commentContainer = document.createElement("div");
+  commentContainer.classList.add("comment__container");
   biggerContainer.appendChild(commentContainer);
-  let imageUrl = document.createElement('img');
-  imageUrl.classList.add("comments__image-url");
-  commentContainer.appendChild(imageUrl);
-  let commentName = document.createElement("h4");
-  commentContainer.appendChild(commentName);
-  commentName.classList.add("comments__person");
-  document.querySelectorAll(".comments__person")[i].innerHTML = commentsArray[i].name;
-  let commentDate = document.createElement("p");
-  commentContainer.appendChild(commentDate);
-  commentDate.classList.add("comments__date");
-  document.querySelectorAll(".comments__date")[i].innerHTML = commentsArray[i].date;
-  let commentText = document.createElement("p");
-  commentContainer.appendChild(commentText);
-  commentText.classList.add("comments__text");
-  document.querySelectorAll(".comments__text")[i].innerHTML = commentsArray[i].comment;
-};
-};
-loopingFunction(commentsArray);
 
+  let imageContainer = document.createElement("div");
+  imageContainer.classList.add("comment__image-container");
+  commentContainer.appendChild(imageContainer);
 
-let form = document.getElementById("comments__form")
+  let headerContainer = document.createElement("div");
+  headerContainer.classList.add("comment__header");
+  commentContainer.appendChild(headerContainer);
 
-form.addEventListener("submit", function displayComment(event){
-  let submitBtn = document.getElementById('submitBtn');
-  event.preventDefault();
-  // let commentContainer = document.createElement('div');
-  // commentContainer.classList.add("comments__container");
-  
-  // parentForm.appendChild(commentContainer);
-  // let imageUrl = document.createElement('img');
-  // imageUrl.classList.add("comments__image-url");
-  // commentContainer.appendChild(imageUrl);
+  let image = document.createElement("div");
+  image.classList.add("comment__header-image");
+  imageContainer.appendChild(image);
 
-  let newName = event.target.name.value;
-  let newComment = event.target.message.value;
-  let dateValue = event.target.message.value;
+  let name = document.createElement("h2");
+  name.classList.add("comment__header-name");
+  name.innerText = array[i]["name"];
+  headerContainer.appendChild(name);
 
-  ////////////
+  let date = document.createElement("p");
+  date.classList.add("comment__header-date");
+  date.innerText = array[i]["date"];
+  headerContainer.appendChild(date);
 
-  
-  
+  let textContainer = document.createElement("div");
+  textContainer.classList.add("comment__text-default");
+  commentContainer.appendChild(textContainer);
 
+  let comment = document.createElement("p");
+  comment.classList.add("comment__text-comment");
+  comment.innerText = array[i]["comment"];
+  textContainer.appendChild(comment);
+  }
+}
+displayComments(commentsArray);
 
+const form = document.querySelector(".comment__input-container");
 
-  // let newContainer = document.createElement('div');
-  // newContainer.classList.add("comments__container");
-  // parentForm.appendChild(newContainer);
-  // let imageUrl = document.createElement('img');
-  // imageUrl.classList.add("comments__image-url");
-  // commentContainer.appendChild(imageUrl);
-  // let newCommentName = document.createElement("h4");
-  // newContainer.appendChild(newCommentName);
-  // newCommentName.classList.add("comments__person");
-  // document.querySelectorAll(".comments__person").innerHTML.newName;
-  // let newCommentDate = document.createElement("p");
-  // newCommentContainer.appendChild(newCommentDate);
-  // newCommentDate.classList.add("comments__date");
-  // document.querySelectorAll(".comments__date")[i].innerHTML.dateValue;
-  // let newCommentText = document.createElement("p");
-  // commentContainer.appendChild(newCommentText);
-  // newCommentText.classList.add("comments__text");
-  // document.querySelectorAll(".comments__text")[i].innerHTML.newComment;
+form.addEventListener("submit", function(event) {
+event.preventDefault();
 
+const addComment = {};
+addComment.name = event.target.name.value;
+addComment.comment = event.target.comment.value;
 
+let biggerContainer = document.querySelector(".comment__comments-array");
 
-  ////////////put the infor in the tag
+let commentContainer = document.createElement("div");
+commentContainer.classList.add("comment__container");
+biggerContainer.appendChild(commentContainer);
 
-  let getDate = new Date()
-  let getMonth = getDate.getMonth()+1; 
-  let getDay = getDate.getDate();
-  let getYear = getDate.getFullYear();
-  let newDate = `${getMonth}/${getDay}/${getYear}`;
-  console.log('date: '+ newDate);
+let imageContainer = document.createElement("div");
+imageContainer.classList.add("comment__image-container");
+commentContainer.appendChild(imageContainer);
 
-  let newCommentObject = {
-    name: newName,
-    date: newDate,
-    comment: newComment    
-  };
+let headerContainer = document.createElement("div");
+headerContainer.classList.add("comment__header");
+commentContainer.appendChild(headerContainer);
 
+let image = document.createElement("div");
+image.classList.add("comment__header-image");
+imageContainer.appendChild(image);
 
+let name = document.createElement("h2");
+name.classList.add("comment__header-name");
+name.innerText = addComment.name;
+headerContainer.appendChild(name);
 
-  
+let getDate = new Date();
+let getDay = String(getDate.getDate()).padStart(2, "0");
+let getMonth = String(getDate.getMonth() + 1).padStart(2, "0");
+let getYear = getDate.getFullYear();
 
-  
-  console.log("this is name: " + newName);
-  console.log("this is comment: " + newComment);
-  // commentName.classList.add("comments__person");
-  // let input = event.target.name.value;
-  // commentName.innerHTML = input;
-  // document.querySelector("input").value = "";
+getDate = `${getMonth}/${getDay}/${getYear}`;
 
-  // createComment(newCommentObject);
-  // console.log(newCommentObject);
-  commentsArray.unshift(
-    {
-    name: newName,
-    date: newDate,
-    comment: newComment
-  });
+let newDate = document.createElement("p");
+newDate.innerText = getDate;
+newDate.classList.add("comment__header-date");
+headerContainer.appendChild(newDate);
 
-  // function createComment(input) {
-    // console.log("input goes here: " + input);
-    // let commentContainer = document.createElement('div');
+let textContainer = document.createElement("div");
+textContainer.classList.add("comment__text-default");
+commentContainer.appendChild(textContainer);
 
-    // let imageUrl = document.createElement('img');
-    // let commentName = document.createElement("h4");
-    // let commentDate = document.createElement("p");
-    // let commentText = document.createElement("p");
-    // commentContainer.classList.add('comments__container');
-    // let newContainer = document.createElement("div");
-    // let newImage = document.createElement("img");
-    // let newCommentName = document.createElement("h4");
-    // let newCommentDate = document.createElement("p");
-    // let newCommentText = document.createElement("p");
-  
-    // biggerContainer.appendChild(commentContainer);
-    // commentContainer.appendChild(imageUrl);
-    // commentContainer.appendChild(commentName); 
-    // commentContainer.appendChild(commentDate);
-    // commentContainer.appendChild(commentText);
-  
-    // commentName.innerText = newName;
-    // commentDate.innerText = newDate;
-    // commentText.innerText = newComment; 
-    
-    // };
-    console.log(commentsArray);
-    
+let comment = document.createElement("p");
+comment.classList.add("comment__text-comment");
+comment.innerText = addComment.comment;
+textContainer.appendChild(comment);
 
-    commentsArray.forEach(element => {
-      let commentContainer = document.createElement('div');
-      let imageUrl = document.createElement('img');
-      let commentName = document.createElement("h4");
-      let commentDate = document.createElement("p");
-      let commentText = document.createElement("p");
-      biggerContainer.classList.add("comments__bigger-container");
-      commentContainer.classList.add("comments__container");
-      imageUrl.classList.add("comments__image-url");
-      commentName.classList.add("comments__person");
-      commentDate.classList.add("comments__date");
-      commentText.classList.add("comments__text");
-      
-      biggerContainer.appendChild(commentContainer);
-      commentContainer.appendChild(imageUrl);
-      commentContainer.appendChild(commentName); 
-      commentContainer.appendChild(commentDate);
-      commentContainer.appendChild(commentText);
-    
-      commentName.innerText = element.name;
-      commentDate.innerText = element.date;
-      commentText.innerText = element.comment; 
-    });
+//Couldn't make it work with unshift. Found this method on Stack Overflow
+let firstComment = document.querySelector(".comment__comments-array");
+firstComment.insertBefore(commentContainer, firstComment.firstChild);
+
+document.querySelector(".comment__input-container").reset();
 });
-
-
-
-
-
-
-
